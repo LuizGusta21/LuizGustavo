5dd3f3bf919e1d6b5aa62cf1e499a4454d3916c9
Luiz Gustavo <87343462+LuizGusta21@users.noreply.github.com>
2023-06-28 00:09:36 -0300

    Add files via upload

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,11 +3,13 @@ const AboutContainer = () => {
     <section>
       <h2>Sobre</h2>
       <p>
-        Meu nome é Luiz Gustavo, tenho 22 anos e sou um apaixonado pela programação. Tudo aconteceu por conta de sempre gostar de ficar no PC, jogando alguns games Fps e Moba, logo em 2018 me veio a curiosidade de como eram feitos os jogos, na empolgação fui pesquisando e entendo algumas lógicas, porém não levei pra frente.
-        Em meados do final de 2021, eu comecei a pesquisar novamente sobre o assunto, estava determinado a entrar em alguma faculdade da área. Começou o ano de 2022 e eu entrei no curso de Engenharia de Software (EAD). Comecei a estudar sobre HTML e CSS. Logo fui procurar saber sobre Javascript e me aperfeiçoando no desenvolvimento web.
-      </p>
-      <p>
-        Hoje já me aperfeiçoei em react, sass  e estou buscando meu aprimoramento em typescript. Estou em busca de uma vaga para poder contribuir com uma missão e adquirir mais conhecimentos.
+        Olá, sou Luiz Gustavo, um entusiasta da programação de 22 anos.
+        Minha paixão por programar nasceu quando comecei a explorar jogos FPS e MOBA e me perguntei como eles eram feitos.
+        Em 2018, comecei a pesquisar e entender as lógicas por trás dos jogos, mas não levei adiante naquela época.
+        Porém, no final de 2021, decidi retomar minha busca e estava determinado a ingressar na área.
+        Em 2022, iniciei o curso de Engenharia de Software (EAD) e comecei a estudar HTML, CSS e, em seguida, me aprofundei no desenvolvimento web,
+        especialmente em React, Sass e agora estou focado em aprimorar minhas habilidades em TypeScript.
+        Estou animado para encontrar uma oportunidade que me permita contribuir com uma missão e expandir ainda mais meus conhecimentos.
       </p>
     </section>
   )
